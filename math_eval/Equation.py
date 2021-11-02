416621a250aa8fcd77781b2a2bff812d11fe866d
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2021-11-02 09:58:53 -0700

    Fix Equation imports of math ufunctions Now ufunctions that don't accept 2 as input (e.g. acos) will still be added to ufunctions and safe_ufunctions.

--- math_eval/Equation.py
+++ math_eval/Equation.py
@@ -1,10 +1,3 @@
-'''Python 2 had a package on the PYPI called Equation that did something like
-safe_compute. This is a revival of that.
-The Equation class is more or less a wrapper around safe_compute with
-some extra math functions thrown in.
-Every single-variable function in Python's built-in "math" module 
-is available for use here.
-'''
 from math import *
 import math
 from .math_eval import *
@@ -14,14 +7,26 @@ one_arg_mathfuncs = {}
 for funcname in dir(math):
     func = globals()[funcname]
     try:
-        func(2)
+        func(2) # if this works, the function accepts one arg
         one_arg_mathfuncs[funcname] = func
-    except:
-        pass
+    except Exception as ex: 
+        # this is most likely either because func requires multiple args
+        # or because it's not a function (e.g., math.tau).
+        if "math domain error" in repr(ex):
+            # the function accepts one arg, but not 2. E.g., acos, asin.
+            one_arg_mathfuncs[funcname] = func
+
 safe_ufunctions.update(one_arg_mathfuncs)
 ufunctions.update(one_arg_mathfuncs)
 
 class Equation:
+    '''Python 2 had a package on the PYPI called Equation that did something like
+safe_compute. This is a revival of that.
+The Equation class is more or less a wrapper around safe_compute with
+some extra math functions thrown in.
+Every single-variable function in Python's built-in "math" module 
+is available for use here.
+'''
     def __init__(self, eqn):
         self.eqn = eqn
         self.expr = compute(eqn, safe = True)
